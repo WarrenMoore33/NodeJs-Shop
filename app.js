3803ac09752d251d13b94669fad238a6d78f6be3
Warren Moore <wmoore@banyancom.com>
2026-09-03 11:55:51 -0400

    adding status to the send and creating a 404 error if page is not found

--- app.js
+++ app.js
@@ -24,5 +24,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+// 404 page not found. If the user tries to access a page that does not exist, this will be the response.
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
+});
+
 // local host where I choose what port. 3000 just sounds the best
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-router.use("/", (req, res, next) => {
+router.get("/", (req, res, next) => {
   res.send(
     "<h1>Hello from Express</h1><div><a href='add-product' style='color: red; font-weight: bold; padding: 10px; background: black; border: 1px solid pink; text-decoration: none;'>Add Product</a></div>",
   );
