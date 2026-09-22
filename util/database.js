8a56976ed48aa91ba14a32976dfb113caa5fc3c1
Warren Moore <wmoore@banyancom.com>
2026-09-22 10:45:07 -0400

    products from mongo db showing

--- util/database.js
+++ util/database.js
@@ -1,8 +1,29 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node_complete", "root", "Banyan77!", {
-  dialect: "mysql",
-  host: "localhost",
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://wcmoore33_db_user:aVnmbRZndx8sMm8j@cluster0.sduinqc.mongodb.net/shop?appName=Cluster0",
+  )
+    .then((client) => {
+      console.log("Connected!");
+      _db = client.db();
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
